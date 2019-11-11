7d1c67da3ec28f7520fa53e4fd8396588897a6c5
jhovin <jhovin.bonifacio@tecsup.edu.pe>
2019-11-11 16:37:48 -0500

    first commit

---
+++ Ejercicio13/viewModels/ITextToSpeech.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio13.viewModels
+{
+    public interface ITextToSpeech
+    {
+        void Speak(string text);
+    }
+}
